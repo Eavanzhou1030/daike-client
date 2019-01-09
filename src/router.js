13bd0ad086ad098866959f11cac0f6f91905faaf
zhouyudong <2870713431@qq.com>
2019-01-09 18:11:08 +0800

    daike client structor build

---
+++ src/router.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/login')
+    },
+    {
+      path: '/home',
+      name: 'home',
+      redirect: '/home/mine',
+      children: [
+        {
+          path: '/home/mine',
+          name: 'mine',
+          component: () => import('./views/mine')
+        },
+        {
+          path: '/home/publish',
+          name: 'publish',
+          component: () => import('./views/publish')
+        },
+        {
+          path: '/home/substitute',
+          name: 'substitute',
+          component: () => import('./views/substitute')
+        },
+        {
+          path: '/home/course',
+          name: 'course',
+          component: () => import('./views/course')
+        }
+      ]
+    }
+  ]
+})
